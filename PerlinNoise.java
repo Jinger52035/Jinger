6ac29c10f6cb3f37f871557f48c9d0be433f06cd
Jinger52035 <1057266013@qq.com>
2020-07-29 18:20:10 +0800

    2.0

---
+++ PerlinNoise.java
@@ -0,0 +1,169 @@
+package Test1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+import javax.swing.JFrame;
+
+public class PerlinNoise extends JFrame{
+	
+	private int w = 500;//窗口宽度
+	private int h = 500;//窗口高度
+	
+	//窗口方法
+	public void launchFrame() {
+		this.setTitle("地图0.4");
+		this.setVisible(true);
+		this.setSize(w, h);
+		this.setLocation(100,100);//窗口的坐标
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				super.windowClosed(e);
+				System.exit(0);
+			}
+		});	
+	}
+	
+	//主方法
+	public static void main(String[] args) {
+		PerlinNoise a = new PerlinNoise();
+		a.launchFrame();
+    }
+    //白噪声函数，是一个二维数组，每个数大于等于0，小于1
+	public float[][] GenerateWhiteNoise(int width,int height,int seed){
+        Random r = new Random(seed);
+        float[][] noise=new float[width][];
+        for (int i = 0; i < width; i++){
+            noise[i] = new float[height];
+        }
+        
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < height; j++){
+                noise[i][j] = (float)r.nextDouble() % 1;
+            }
+        }
+        return noise;
+    }
+	
+	float Interpolate(float x0, float x1, float alpha){
+        return x0 * (1 - alpha) + alpha * x1;
+    }
+	public float[][] GenerateSmoothNoise(float[][] baseNoise, int octave)
+    {
+        int width = baseNoise.length;
+        int height = baseNoise[0].length;
+
+        float[][] smoothNoise = new float[width][];
+        for (int i = 0; i < width; i++)
+        {
+            smoothNoise[i] = new float[height];
+        }
+        int samplePeriod = 1 << octave;
+        float sampleFrequency = 1.0f / samplePeriod;
+        for (int i = 0; i < width; i++)
+        {
+            //calculate the horizontal sampling indices
+            int sample_i0 = (i / samplePeriod) * samplePeriod;
+            int sample_i1 = (sample_i0 + samplePeriod) % width; //wrap around
+            float horizontal_blend = (i - sample_i0) * sampleFrequency;
+
+            for (int j = 0; j < height; j++)
+            {
+                //calculate the vertical sampling indices
+                int sample_j0 = (j / samplePeriod) * samplePeriod;
+                int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
+                float vertical_blend = (j - sample_j0) * sampleFrequency;
+
+                //blend the top two corners
+                float top = Interpolate(baseNoise[sample_i0][sample_j0],
+                   baseNoise[sample_i1][sample_j0], horizontal_blend);
+
+                //blend the bottom two corners
+                float bottom = Interpolate(baseNoise[sample_i0][sample_j1],
+                   baseNoise[sample_i1][sample_j1], horizontal_blend);
+
+                //final blend
+                smoothNoise[i][j] = Interpolate(top, bottom, vertical_blend);
+            }
+        }
+
+        return smoothNoise;
+    }
+
+	public float[][] GeneratePerlinNoise(float[][] baseNoise, int octaveCount){
+        int width = baseNoise.length;
+        int height = baseNoise[0].length;
+
+        float[][][] smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
+        float persistance = 0.5f;
+        //generate smooth noise
+        for (int i = 0; i <octaveCount; i++)
+        {
+            smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
+        }
+
+        float[][] perlinNoise = new float[width][];
+        for (int i = 0; i < width; i++)
+        {
+            perlinNoise[i] = new float[height];
+        }
+        float amplitude = 1.0f;
+        float totalAmplitude = 0.0f;
+
+        //blend noise together
+        for (int octave = octaveCount - 1; octave >= 0; octave--)
+        {
+           amplitude *= persistance;
+           totalAmplitude += amplitude;
+
+           for (int i = 0; i <width; i++)
+           {
+              for (int j = 0; j < height; j++)
+              {
+                 perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+              }
+           }
+        }
+        //normalisation
+       for (int i = 0; i <width; i++)
+       {
+          for (int j = 0; j <height; j++)
+          {
+             perlinNoise[i][j] /= totalAmplitude;
+          }
+       }
+       return perlinNoise;
+    }
+	int o=10;
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		int a = 1;//定义像素块大小
+		int s = (int)(Math.random()*20000);//随机种子
+		
+		float[][] noises = GenerateWhiteNoise(500, 500,6527);
+        float[][] smooth = GenerateSmoothNoise(noises, 5);
+        float[][] perlins = GeneratePerlinNoise(noises, o);
+		
+		//嵌套循环赋值
+		for (int i = 0; i < w/a; i++)
+        {
+            for (int j = 0; j < h/a; j++)
+            {
+                int co = (int)(perlins[i][j] * 256);
+                Color c = new Color(co,co,co);//随机颜色
+                g.setColor(c);//设置颜色
+                g.fillRect(i*a, j*a, a, a);//绘制实心方块 前两个是坐标，后两个是大小
+            }
+        }
+		
+	
+	}
+	
+    
+	
+}
